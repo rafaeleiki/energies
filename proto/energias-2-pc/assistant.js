0cf3bc1af9f07645a9dcf01479c1f0c3a81bc819
Rafael Eiki <rafaeleiki@yahoo.com.br>
2018-08-23 15:19:02 -0300

    Creates basic text replicator

---
+++ proto/energias-2-pc/assistant.js
@@ -0,0 +1,72 @@
+(function() {
+    'use strict';
+
+    let SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+    let speech = new SpeechSynthesisUtterance();
+    let recognition = new SpeechRecognition();
+    let container = document.getElementById('container');
+    let speaking = false;
+    let text = '';
+
+    function prepareSpeech() {
+        findVoice('BR');
+
+        window.speechSynthesis.onvoiceschanged = () => {
+            findVoice('BR');
+        };
+
+        speech.onstart = function() {
+            container.classList.add('speaking');
+            recognition.abort();
+            speaking = true;
+        };
+
+        speech.onend = function() {
+            container.classList.remove('speaking');
+            speaking = false;
+        }
+    }
+
+    function findVoice(language) {
+        let voices = window.speechSynthesis.getVoices();
+
+        voices.forEach(function (voice) {
+            if (voice.lang.indexOf(language) > 0) {
+                speech.voice = voice;
+                speech.voiceURI = voice.voiceURI;
+                speech.lang = voice.lang;
+                speech.localService = true;
+            }
+        });
+    }
+
+    function prepareRecognition() {
+        recognition.continuous = false;
+        recognition.lang = 'pt-BR';
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
+
+        recognition.onresult = function(event) {
+            let text = event.results[0][0].transcript;
+            speech.text = text;
+            window.speechSynthesis.speak(speech);
+        };
+
+        setInterval(() => {
+            if (!speaking) {
+                try {
+                    recognition.start();
+                } catch (e) {}
+            }
+        }, 500);
+
+        recognition.start();
+    }
+
+    function start() {
+        prepareSpeech();
+        prepareRecognition();
+    }
+
+    start();
+})();
